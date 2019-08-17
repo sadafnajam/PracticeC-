4218f61f7b9775cf09f3dcae6d2ece6fd31aa792
Sadaf Najam <sadaf.najam91@gmail.com>
2019-08-17 16:08:34 -0400

    Initial Commit

---
+++ PracticeC++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PracticeC++
+//
+//  Created by Sadaf Najam on 2019-08-17.
+//  Copyright © 2019 Sadaf Najam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
